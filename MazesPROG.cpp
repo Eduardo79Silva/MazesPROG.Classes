7ecaac392268fb1996bee321a2eaa0c0ef02ecec
Eduardo79Silva <80106028+Eduardo79Silva@users.noreply.github.com>
2021-04-23 12:45:31 +0100

    Update 1

--- MazesPROG.cpp
+++ MazesPROG.cpp
@@ -5,21 +5,40 @@ using namespace std;
 
 int main()
 {
+    bool playerAlive = true;
+    bool mazeOpened = false;
     string mazeNum;
-    cout << "Choose the maze's number: "; cin >> mazeNum;
-    string line;
-    ifstream myfile("MAZE_" + mazeNum + ".txt");
-    if (myfile.is_open())
-    {
-        while (myfile.good())
-        {
-            getline(myfile, line);
-            cout << line << endl;
-        }
-        myfile.close();
-    }
+    string playerMove;
 
-    else cout << "Unable to open file";
+   while (playerAlive) {
+        int lineCount = 0;
+        while (mazeOpened == false) {
+            cout << "Choose the maze's number: "; cin >> mazeNum;
+
+        }
+        string line;
+        ifstream myfile("MAZE_" + mazeNum + ".txt");
+        if (myfile.is_open())
+        {
+            while (myfile.good())
+            {
+                getline(myfile, line);
+                if (lineCount > 0) {
+                    cout << line << endl;
+                }
+                lineCount++;
+                  
+            }
+            myfile.close();
+            mazeOpened = true;
+        }
+
+       
+        cout << "Direction to move: "; cin >> playerMove;
+
+
+
+    }
 
     return 0;
 }
