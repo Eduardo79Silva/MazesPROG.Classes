71a7d88578d3fb6b72b672b58a7152b4daf6ff91
Naz <up202005283@up.pt>
2021-04-28 20:59:43 +0100

    .cpp --> solution
    Mudei para uma solution do visual studio senão não se conseguia dar debug

---
+++ MazesPROG/MazesPROG2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    bool playerAlive = true;
+    bool mazeOpened = false;
+    string mazeNum;
+    string playerMove;
+
+    while (playerAlive) {
+        int lineCount = 0;
+        if (mazeOpened == false) {
+            cout << "Choose the maze's number: "; cin >> mazeNum;
+        }
+
+        string line;
+        ifstream myfile("MAZE_" + mazeNum + ".txt");
+        if (myfile.is_open())
+        {
+
+            while (myfile.good())
+            {
+                getline(myfile, line);
+                if (lineCount > 0) {
+                    cout << line << endl;
+                }
+                lineCount++;
+
+            }
+            myfile.close();
+            mazeOpened = true;
+        }
+
+
+        cout << "Direction to move: "; cin >> playerMove;
+
+
+
+    }
+
+    return 0;
+}
