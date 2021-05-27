54221722d42a5ee4be72020eb31e15dfd63cca9f
Naz <up202005283@up.pt>
2021-05-27 11:27:32 +0100

    .

--- PROG2/Maze.cpp
+++ PROG2/Maze.cpp
@@ -4,10 +4,13 @@
 
 Maze::Maze(int numRows, int numCols)
 {
+	numCols = nCols;
+	numRows = nRows;
 }
 
 bool Maze::addPost(const Post& post, const Position& position)
 {
+	posts.push_back(post);
 	return false;
 }
 
--- PROG2/Player.cpp
+++ PROG2/Player.cpp
@@ -2,6 +2,18 @@
 
 Player::Player(int row, int col, char symbol)
 {
+	this->row = row;
+	this->col = col;
+	this->symbol = symbol;
+	alive = true;
+}
+
+Player::Player()
+{
+	row = -1;
+	col = -1;
+	symbol = 'H';
+	alive = true;
 }
 
 int Player::getRow() const
@@ -30,5 +42,7 @@ void Player::setAsDead()
 
 bool Player::move(Movement delta)
 {
+	row += delta.dRow;
+	col += delta.dCol;
 	return false;
 }
